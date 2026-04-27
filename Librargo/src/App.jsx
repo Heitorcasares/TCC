9d8d55d3270d2b40439e08223498227ff18d206a
Heitorcasares <suzanacasares@gmail.com>
2026-04-27 14:40:41 -0300

    Configurando barra de pesquisa

--- Librargo/src/App.jsx
+++ Librargo/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
-import logo from 'C:/Users/CAMARGO/Downloads/libras-removebg-preview.png'
+import logo from "C:/Users/Lab 06-Micro 10/Downloads/libras-removebg-preview.png"
+import usuario from "C:/Users/Lab 06-Micro 10/Downloads/user-icon-on-transparent-background-free-png-removebg-preview.png"
 import './App.css'
 
 
@@ -13,6 +14,25 @@ function App() {
             <li><img src={logo} style={{width: "60px", height: "60px", paddingTop: "12px", margin: "0", float: "left"}}/></li>
             <li><h3><a href="#">Home</a></h3></li>
             <li><h3><a href="#">Comunidades</a></h3></li>
+            <li><div class="group">
+                  <svg viewBox="0 0 24 24" aria-hidden="true" class="search-icon">
+                    <g>
+                      <path
+                        d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
+                      ></path>
+                    </g>
+                  </svg>
+
+                  <input
+                    id="query"
+                    class="input"
+                    type="search"
+                    placeholder="Search..."
+                    name="searchbar"
+                  />
+                </div>
+            </li>
+            <li><img src={usuario}  style={{width: "60px", height: "37px", paddingTop: "3px", margin: "0", float: "right"}}/></li>
           </ul>
         </nav>
       </div>    
