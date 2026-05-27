c7f3bc0a618342979f19bd6b9361c9d8728accd6
Heitorcasares <suzanacasares@gmail.com>
2026-05-27 11:01:49 -0300

    Progredindo no nosso TCC

--- maos.py
+++ maos.py
@@ -1,7 +1,7 @@
 import cv2
 import mediapipe as mp
 
-video = cv2.VideoCapture(1)
+video = cv2.VideoCapture(0)
 
 hand = mp.solutions.hands
 Hand = hand.Hands(max_num_hands=1)
@@ -21,6 +21,23 @@ while True:
                 cx, cy = int(cord.x*w), int(cord.y*h)
                 #cv2.putText(img, str(id), (cx, cy+10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
                 pontos.append((cx, cy))
+
+
+        dedos = [8, 12, 16, 20]
+        contador = 0
+
+        if points:
+            if pontos[4][0] < pontos[2][0]:
+                contador +=1
+            for x in dedos:
+                if pontos[x][1] < pontos[ x-2 ][1]:
+                    contador +=1
+
+        print(contador)
+
+
+
+
 
     cv2.imshow("IMAGEM", img)
     cv2.waitKey(1)
